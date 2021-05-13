51300129fa6bd80b0d564b21cf417352a6d1ee84
udberg <johnuduak066@gmail.com>
2021-05-13 13:57:41 +0100

    Import images to background code

---
+++ src/changeBackground.js
@@ -0,0 +1,6 @@
+import clearSky from '../img/cl_sky.jpg';
+import cloudy from '../img/fw_clouds.jpg';
+import rain from '../img/lt_rain.jpg';
+import mist from '../img/mist.jpg';
+import storm from '../img/th_storm.jpg';
+import winter from '../img/winter.jpg';
